d704175999f76b9c0e0fff8551c3d8ad32d10e85
Marcus <marcusrichardson7838@gmail.com>
2026-01-06 10:44:49 +0000

    new query practicing rank () over ()

--- queries.py
+++ queries.py
@@ -241,6 +241,21 @@ def find_top_five_salaries() -> list[dict[str, Any]]:
     )
 
 
+def salary_ranking() -> list[dict[str, Any]]:
+    return fetch_result(
+        """
+        select e.employee_id,
+               d.name,
+               s.amount,
+               s.effective_date,
+               rank () over (partition by d.name order by s.amount desc) as salary_ranking
+            from salaries s
+            left join employees e on e.employee_id = s.employee_id
+            left join departments d on e.department_id = d.department_id
+        """
+    )
+
+
 def main() -> None:
     pass
 
